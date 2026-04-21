a95e1f4c7a8efc950c0914fd91bc5225111a3a2a
mikita <patronikm@gmail.com>
2026-04-21 16:40:07 +0200

    feat: add my_malloc and my_free logic

--- src/allocator.c
+++ src/allocator.c
@@ -1,10 +1,48 @@
 #include "allocator.h"
 #include "internal.h"
+#include <stdio.h>
 
-void *my_malloc(size_t size){
-    
+static int initialized = 0;
+
+void *my_malloc(size_t size) {
+  if (!initialized) {
+    if (mm_init() == -1)
+      return NULL;
+    initialized = 1;
+  }
+
+  if (size == 0)
+    return NULL;
+
+  size_t asize = (size + OVERHEAD + (ALIGNMENT - 1)) &
+                 ~(ALIGNMENT - 1); // zaokraglanie do 16
+
+  void *bp = find_fit(asize);
+
+  if (bp != NULL) {
+    place(bp, asize);
+    return bp;
+  }
+
+  size_t extendsize = (asize > CHUNKSIZE) ? asize : CHUNKSIZE;
+
+  bp = extend_heap(extendsize);
+
+  if (bp == NULL)
+    return NULL;
+
+  place(bp, asize);
+  return bp;
 }
 
-void my_free(void *_Nullable ptr){
+void my_free(void *ptr) {
+  if (ptr == NULL)
+    return;
 
+  size_t size = GET_SIZE(HDRP(ptr));
+
+  PUT(HDRP(ptr), PACK(size, 0));
+  PUT(FTRP(ptr), PACK(size, 0));
+
+  coalesce(ptr);
 }
