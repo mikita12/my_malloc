fd41bef023883f8f36d8810f273000031b6efe77
mikita <patronikm@gmail.com>
2026-04-21 16:40:31 +0200

    feat: add place() function

--- src/block.c
+++ src/block.c
@@ -1,8 +1,11 @@
 #include "internal.h"
+#include <stddef.h>
+#include <stdio.h>
 
 extern char *heap_listp;
 
 void *find_fit(size_t asize){ //first fit
+
     char *bp = NEXT_BLKP(heap_listp);
     
     while(GET_SIZE(HDRP(bp))){
@@ -18,5 +21,23 @@ void *find_fit(size_t asize){ //first fit
 }
 
 void place(void* bp, size_t asize){
+    size_t csize = GET_SIZE(HDRP(bp));
 
+    if(csize - asize >= DSIZE){
+        
+        PUT(HDRP(bp),PACK(asize,1));
+        PUT(FTRP(bp),PACK(asize,1));
+        
+        char* bp2 = NEXT_BLKP(bp);
+        
+        PUT(HDRP(bp2),PACK(csize-asize,0));
+        PUT(FTRP(bp2),PACK(csize-asize,0));
+
+        return;
+    }
+
+    PUT(HDRP(bp),PACK(csize,1));
+    PUT(FTRP(bp),PACK(csize,1));
+
+    return;
 }
