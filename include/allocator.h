76187e3717314c0f3c0ea4d102e672542d2dd51b
mikita <patronikm@gmail.com>
2026-04-21 16:46:34 +0200

    feat: add missing functions declarations

--- include/allocator.h
+++ include/allocator.h
@@ -7,5 +7,9 @@
 int mm_init(void);
 void* my_malloc(size_t size);
 void my_free(void* ptr);
+void *coalesce(void *bp);
+void *find_fit(size_t asize);
+void place(void* bp, size_t asize);
+void *extend_heap(size_t size);
 
 #endif
