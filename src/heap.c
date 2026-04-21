00cccc7ddb7fcaf16a591750d98257d262abced9
mikita <patronikm@gmail.com>
2026-04-21 16:41:17 +0200

    fix: fixed allingment,heap extend issues

--- src/heap.c
+++ src/heap.c
@@ -1,91 +1,90 @@
 #include "allocator.h"
 #include "internal.h"
+#include <stdbool.h>
+#include <stdint.h>
+#include <stdio.h>
 
 //| padding | prologue header | prologue footer | epilogue header |
 
-static char *heap_listp; //heap start
+char *heap_listp; // heap start
 
-int mm_init(){
+int mm_init() {
 
-    char *heap = sbrk(4*WSIZE);
-    if(heap == (void *)-1)
-        return -1;
-    
-    PUT(heap,0); //padding
+  char *heap = sbrk(4 * WSIZE);
+  if (heap == (void *)-1)
+    return -1;
 
-    PUT(heap + (1 * WSIZE), PACK(DSIZE,1)); //prologue header
+  PUT(heap, 0); // padding
 
-    PUT(heap + (2 * WSIZE), PACK(DSIZE,1)); //prologue footer
+  PUT(heap + (1 * WSIZE), PACK(DSIZE, 1)); // prologue header
 
-    PUT(heap + (3 * WSIZE), PACK(0,1)); //epilogue header
+  PUT(heap + (2 * WSIZE), PACK(DSIZE, 1)); // prologue footer
 
-    heap_listp = heap  + (2 * WSIZE);
+  PUT(heap + (3 * WSIZE), PACK(0, 1)); // epilogue header
 
-    return 0;
+  heap_listp = heap + (2 * WSIZE);
+  return 0;
 }
 
-void *coalesce(void *bp){
+void *coalesce(void *bp) {
 
-    size_t size = GET_SIZE(HDRP(bp));
-    size_t sizeBuf;
+  size_t size = GET_SIZE(HDRP(bp));
 
-    bool prev = GET_ALLOC(HDRP(PREV_BLKP(bp)));
-    bool next = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
+  bool prev = GET_ALLOC(HDRP(PREV_BLKP(bp)));
+  bool next = GET_ALLOC(HDRP(NEXT_BLKP(bp)));
 
-    if(prev && next){
-        return bp;
-    }else if(prev && !next){
+  if (prev && next) {
+    return bp;
+  } else if (prev && !next) {
 
-        size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
-        PUT(HDRP(bp),PACK(size,0));
-        PUT(FTRP(bp),PACK(size,0));
+    size += GET_SIZE(HDRP(NEXT_BLKP(bp)));
+    PUT(HDRP(bp), PACK(size, 0));
+    PUT(FTRP(bp), PACK(size, 0));
 
-    }else if(!prev && next){
+  } else if (!prev && next) {
 
-        size += GET_SIZE(HDRP(PREV_BLKP(bp)));
-        bp = PREV_BLKP(bp);
+    size += GET_SIZE(HDRP(PREV_BLKP(bp)));
+    bp = PREV_BLKP(bp);
 
-        PUT(HDRP(bp),PACK(size,0));
-        PUT(FTRP(bp),PACK(size,0));
+    PUT(HDRP(bp), PACK(size, 0));
+    PUT(FTRP(bp), PACK(size, 0));
 
-    }else{
+  } else {
 
-        char *bp_next;
-        char *bp_prev;
+    char *bp_next;
+    char *bp_prev;
 
-        bp_next = NEXT_BLKP(bp);
-        bp_prev = PREV_BLKP(bp);
+    bp_next = NEXT_BLKP(bp);
+    bp_prev = PREV_BLKP(bp);
 
-        size += GET_SIZE(HDRP(bp_next)) + GET_SIZE(HDRP(bp_prev));
+    size += GET_SIZE(HDRP(bp_next)) + GET_SIZE(HDRP(bp_prev));
 
-        PUT(HDRP(bp_prev),PACK(size,0));
-        PUT(FTRP(bp_next),PACK(size,0));
+    PUT(HDRP(bp_prev), PACK(size, 0));
+    PUT(FTRP(bp_next), PACK(size, 0));
 
-        bp = bp_prev;
-    }
+    bp = bp_prev;
+  }
 
-    return (char *)(bp); //adres wskazujacy na payload polaczonego bloku
+  return (char *)(bp); // adres wskazujacy na payload polaczonego bloku
 }
 
-void *extend_heap(size_t size ){
+void *extend_heap(size_t size) {
+  size = (size + (ALIGNMENT - 1)) & ~(ALIGNMENT - 1); // damn
 
-    size = (size + (ALIGNMENT - 1)) & ~(ALIGNMENT - 1); //damn
+  char *bp = sbrk(size);
+  if (bp == (void *)-1)
+    return NULL;
 
-    char *bp = sbrk(size);
-    if(bp == (void *)-1)
-        return NULL;
+  PUT(HDRP(bp), PACK(size, 0));
+  
+  PUT(FTRP(bp), PACK(size, 0));
 
-    PUT(bp, PACK(size,0));
+  PUT(HDRP(NEXT_BLKP(bp)), PACK(0, 1));
 
-    PUT(bp + size - WSIZE, PACK(size,0));
-
-    PUT(bp + size, PACK(0,1));
-
-    return coalesce(bp + WSIZE);
+  return coalesce(bp);
 }
 
 //|   FREE  |ALLOCATED|   FREE  |
 //|ALLOCATED|ALLOCATED|   FREE  |
 //|   FREE  |ALLOCATED|ALLOCATED|
 //|ALLOCATED|ALLOCATED|ALLOCATED|
-
